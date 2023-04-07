b4b07a5147169107e77f97548c8d4bb85cfe52a0
Andrés <andresrom@gmail.com>
2023-04-07 14:26:14 -0300

    Deploying...

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite';
+	import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-    plugins: [
+    	server: {
+        host: '0.0.0.0',
+        hmr: {
+            host: 'localhost'
+        }
+	},
+	plugins: [
         vue({
             template: {
                 compilerOptions: {
