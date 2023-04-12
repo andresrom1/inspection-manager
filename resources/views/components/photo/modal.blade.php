8d93c739600713f21c736eeb11fe4b4b79fb2f11
andresrom1 <andresrom@gmail.com>
2023-04-12 16:15:02 -0300

    Deploying...

--- resources/views/components/photo/modal.blade.php
+++ resources/views/components/photo/modal.blade.php
@@ -12,7 +12,7 @@
 
                     @foreach ($photos as $photo)
                         <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                            <img src="{{ asset($photo->url) }}" alt="">
+                            <img src="{{ asset($photo->url) }}" class="absolute block max-w-full h-auto -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="">
                         </div>
                     @endforeach
                     
