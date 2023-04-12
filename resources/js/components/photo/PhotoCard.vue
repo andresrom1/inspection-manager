3c8ea5929c42510762c334d8e6a49f030fac4cf4
andresrom1 <andresrom@gmail.com>
2023-04-12 13:42:54 -0300

    Deploying...

--- resources/js/components/photo/PhotoCard.vue
+++ resources/js/components/photo/PhotoCard.vue
@@ -19,7 +19,7 @@
                             <span class="sr-only">Loading...</span>
                         </div>
 
-                        <div for="camera-front" class="dz-clickable flex border-2 px-2 py-2 bg-gray-50 rounded-xl mt-5 items-center" :class="{hidden: step.spinner }">
+                        <div for="camera-front" class="flex border-2 px-2 py-2 bg-gray-50 rounded-xl mt-5 items-center" :class="{hidden: step.spinner }">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 stroke-gray-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z" />
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z" />
--- resources/views/components/photo/modal.blade.php
+++ resources/views/components/photo/modal.blade.php
@@ -12,9 +12,8 @@
 
                     @foreach ($photos as $photo)
                         <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                            <img src="{{ asset($photo->url) }}" class="absolute block max-w-full h-auto -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="">
+                            <img src="{{ asset($photo->url) }}" alt="">
                         </div>
-                        {{ asset($photo->url) }}
                     @endforeach
                     
                 </div>
